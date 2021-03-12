8234265dfcc51b17eed63ebed9ec191ac2ff6aa0
GauravGithubbox <76477015+GauravGithubbox@users.noreply.github.com>
2021-03-12 17:14:33 +0530

    LambdaAssignment fourth Commit by Gaurav Srivastav

--- src/main/java/com/knoldus/RangeProduction.java
+++ src/main/java/com/knoldus/RangeProduction.java
@@ -5,11 +5,11 @@ import java.util.Scanner;
 
 public class RangeProduction {
     public static void main(String[] args) {
-        CalculatesProductionOfAllNumberBetweenRange obj = new CalculatesProductionOfAllNumberBetweenRange();
+        CalcProductionBetweenRange obj = new CalcProductionBetweenRange();
         Scanner scanner = new Scanner(System.in);
         System.out.println("Please Insert First and Second Range=> ");
-         int FirstRange = (int) scanner.nextLong();
-         int SecondRange = (int) scanner.nextLong();
+         String  FirstRange =  scanner.nextLine();
+         String SecondRange =  scanner.nextLine();
 
        System.out.println(obj.Calculation(FirstRange,  SecondRange));
     }
