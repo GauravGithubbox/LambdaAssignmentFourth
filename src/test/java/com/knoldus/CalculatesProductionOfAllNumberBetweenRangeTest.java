53e449079a06e524a44ed03ed0372e062856da2e
GauravGithubbox <76477015+GauravGithubbox@users.noreply.github.com>
2021-03-12 17:15:28 +0530

    LambdaAssignment fourth Commit by Gaurav Srivastav

--- src/test/java/com/knoldus/CalculatesProductionOfAllNumberBetweenRangeTest.java
+++ src/test/java/com/knoldus/CalculatesProductionOfAllNumberBetweenRangeTest.java
@@ -7,22 +7,25 @@ class CalculatesProductionOfAllNumberBetweenRangeTest {
 
     @Test
     public void RangMultiplicationTest() {
-        CalculatesProductionOfAllNumberBetweenRange obj = new CalculatesProductionOfAllNumberBetweenRange();
-        int val = obj.Calculation(0,1);
+
+        int val = CalcProductionBetweenRange.Calculation("0","1");
         Assertions.assertEquals(0, val);
     }
 
     @Test
     public void RangMultiplicationTest1() {
-        CalculatesProductionOfAllNumberBetweenRange obj = new CalculatesProductionOfAllNumberBetweenRange();
-        int val = obj.Calculation(2,2);
+        int val = CalcProductionBetweenRange.Calculation("2","2");
         Assertions.assertEquals(2, val);
     }
     @Test
     public void RangMultiplicationTest3() {
-        CalculatesProductionOfAllNumberBetweenRange obj = new CalculatesProductionOfAllNumberBetweenRange();
-        int val = obj.Calculation(1,4);
+         int val = CalcProductionBetweenRange.Calculation("1","4");
         Assertions.assertEquals(24, val);
+    }
+
+    @Test
+    public void CheckForException() {
+          Assertions.assertThrows(NumberFormatException.class,()->CalcProductionBetweenRange.Calculation("1","Four") );
     }
 
 }
