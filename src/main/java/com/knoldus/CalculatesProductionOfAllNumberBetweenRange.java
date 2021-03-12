329227582ebe04bd5ed0d430c3990a0015810e1a
GauravGithubbox <76477015+GauravGithubbox@users.noreply.github.com>
2021-03-12 17:12:44 +0530

    Update CalculatesProductionOfAllNumberBetweenRange.java

--- src/main/java/com/knoldus/CalculatesProductionOfAllNumberBetweenRange.java
+++ src/main/java/com/knoldus/CalculatesProductionOfAllNumberBetweenRange.java
@@ -3,14 +3,19 @@ package com.knoldus;
 import java.util.function.LongBinaryOperator;
 import java.util.stream.LongStream;
 
-public class CalculatesProductionOfAllNumberBetweenRange {
+class CalcProductionBetweenRange {
 
-    public static int Calculation(int input1, int input2) {
+    public static int Calculation(String input1, String input2) {
         LongBinaryOperator operator = (x, y) -> LongStream.rangeClosed(x, y)
                 .reduce((num1, num2) -> num1 * num2)
-                .orElse(1l);
-       int result = (int) operator.applyAsLong(input1, input2);
-        return  result;
+                .orElse(1L);
+        try {
+            int result = (int) operator.applyAsLong(Integer.parseInt(input1), Integer.parseInt(input2));
+            return result;
+        }catch (Exception e) {
+            System.out.println(" Only Integer Number is Valid  " + e);
+            throw new NumberFormatException();
+        }
 
     }
 }
